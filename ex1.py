4ee4067e8fd5d93dfde5ed6efb02160a682391f8
alancs23 <alancse17@gmail.com>
2014-01-25 06:28:15 +0000

    got glance client

--- ex1.py
+++ ex1.py
@@ -1,4 +1,5 @@
-from keystoneclient.v2_0 import client
+import keystoneclient.v2_0.client as ksclient 
+import glanceclient as gcclient
 
 def getCredentialInfo():
     info = {}
@@ -10,5 +11,13 @@ def getCredentialInfo():
 
 if __name__ == '__main__':
     credentialInfo = getCredentialInfo()
-    keystoneCli = client.Client(**credentialInfo)
-    print keystoneCli.tenants.list()
+    keystoneCli = ksclient.Client(**credentialInfo)
+    #print keystoneCli.tenants.list()
+    gcEndpoint = keystoneCli.service_catalog.get_urls(service_type='image')[0]
+    glanceCli = gcclient.Client('1', gcEndpoint, token=keystoneCli.auth_token)
+    images = glanceCli.images.list()
+    for image in images:
+        if not image.name.find('ubuntu') == -1:
+            print image
+            
+    
